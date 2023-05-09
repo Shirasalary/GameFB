9c6068c67d9d2202f266c1c1b59494be769f18a2
Shira Salary <shira200731@gmail.com>
2023-05-10 00:30:46 +0300

    s

--- src/Constants.java
+++ src/Constants.java
@@ -8,9 +8,16 @@ public class Constants {
 
     public static final Font myFont = new Font("Gisha", Font.BOLD,24);
 
-    public static final int START_SPEED = 1 , TIME_TO_SPEED = 3000 ;
+    public static final int BIRD_SPEED = 3 , CREATE_OBSTACLE = 10 , DISTANCE_TO_ACCURACY = 15 ;
 
     public static final int BIRD_WIDTH = 70, BIRD_HEIGHT = 70;
+
+    public static final int WIDTH_DISTANCE_OBSTACLE = 165 , BETWEEN_DISTANCE_OBSTACLE = 20;
+
+    public static final int OBSTACLE_WIDTH = 60 , MIN_OBSTACLE_HEIGHT = 80 , MIN_OBSTACLE_FOR_PAINT = 3 ;
+
+    public static final int NECESSARY_STOP_TIME = 30 , Y_UP_OBSTACLE = 0 ;
+
 
 
 
--- src/MenuGame.java
+++ src/MenuGame.java
@@ -1,13 +1,16 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class MenuGame extends JPanel {
 
     private JButton startGame;
+    private ImageIcon image;
 
     public MenuGame(int x, int y, int width, int height){
 
         this.setBounds(x,y,width,height);
         this.setLayout(null);
+        this.image = new ImageIcon("menuBackground.png");
         this.startGame = Utils.newButton("Start Game",
                 (Constants.WINDOW_WIDTH /4)*3 -(Constants.BUTTON_WIDTH/2),
                 (Constants.WINDOW_HEIGHT /4)*3);
@@ -18,10 +21,18 @@ public class MenuGame extends JPanel {
            GameScene gameScene = new GameScene(Constants.X_WINDOW, Constants.Y_WINDOW, Constants.WINDOW_WIDTH, Constants.WINDOW_HEIGHT);
            this.add(gameScene);
            this.startGame.setVisible(false);
+           gameScene.requestFocus();
         });
 
 
         this.setVisible(true);
+
+    }
+
+    public void paintComponent(Graphics graphics)
+    {
+        super.paintComponent(graphics);
+        graphics.drawImage(this.image.getImage(),this.getX(),this.getY(),this.getWidth(), this.getHeight(),null );
 
     }
 
